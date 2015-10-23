5524ad19b34fb180196517b3e8c62b7362de32d9
Victor Oza <Victor Oza>
2015-10-23 03:00:54 -0400

    changed to file

--- webcrawl.go
+++ webcrawl.go
@@ -28,7 +28,9 @@ func count(name, url string) {
 	fmt.Printf("%s %d [%.2fs]\n", name, n, time.Since(start).Seconds())
 }
 func do(f func(Comp)) {
-	input, err := os.Open("companies.article")
+	d1 := []byte("{\"Name\": \"Google\", \"Year\": 1998, \"URL\": \"http://google.com/\"}\n{\"Name\": \"Facebook\", \"Year\": 2004, \"URL\": \"http://facebook.com/\"}\n{\"Name\": \"Apple\", \"Year\": 1976, \"URL\": \"http://apple.com/\"}\n{\"Name\": \"Microsoft\", \"Year\": 1975, \"URL\": \"http://microsoft.com/\"}")//, 
+	err := ioutil.WriteFile("test.json", d1, 0644)
+	input, err := os.Open("test.json")
 	if err != nil {
 		log.Fatal(err)
 	}
